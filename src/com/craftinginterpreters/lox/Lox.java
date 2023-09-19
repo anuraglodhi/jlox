593902cead6b3b568f4b1e795048ea2717f51c5e
Anurag Lodhi <anuraglodhi63@gmail.com>
2023-09-19 14:56:03 +0530

    section 4.5.2 Operators

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -7,8 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
-
+import com.craftinginterpreters.lox.Scanner;
 public class Lox {
     static boolean hadError = false;
 
