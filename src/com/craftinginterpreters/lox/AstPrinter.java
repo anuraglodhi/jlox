93dd2159207e83cfa19ec5000bb0b8885b48b062
Anurag Lodhi <anuraglodhi63@gmail.com>
2023-09-28 18:17:04 +0530

    8.2.2 Parsing variables - has errors!!

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -26,6 +26,11 @@ class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override
+    public String visitVariableExpr(Expr.Variable var) {
+        return parenthesize(var.name.lexeme);
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
